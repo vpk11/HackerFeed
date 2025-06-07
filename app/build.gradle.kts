4a40b4d161bc57573da5ea7fc1489e4b6132ce30
vpk11 <devvpk@gmail.com>
2025-06-07 17:12:22 +0200

    bump: versionCode to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.vpk.hackerfeed"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
