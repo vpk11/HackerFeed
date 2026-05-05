91cc4b30e008633fea717584296b350ca6c1ed31
vpk11 <vishnuprasad.kamalon@gmail.com>
2026-05-05 20:21:15 +0200

    chore(build): bump version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.vpk.hackerfeed"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.1.1"
+        versionCode = 5
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
